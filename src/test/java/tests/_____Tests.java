5cb6b52c26185dbe8fff7e88e506d52cb197e0da
AndreyFortunatov <andrey.fortunatov@gmail.com>
2020-06-24 20:45:41 +0300

    пустой тест

--- src/test/java/tests/_____Tests.java
+++ src/test/java/tests/_____Tests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Tag("")
-public class TinkoffTests extends TestBase {
+public class _____Tests extends TestBase {
 
     @Test
     @DisplayName("")
