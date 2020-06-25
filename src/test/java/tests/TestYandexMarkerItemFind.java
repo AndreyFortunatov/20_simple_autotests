b71550b3c081cbdcb31cd84b8cd83fd5da873366
AndreyFortunatov <andrey.fortunatov@gmail.com>
2020-06-25 18:29:42 +0300

    Новый тест на Маркет

---
+++ src/test/java/tests/TestYandexMarkerItemFind.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+
+
+
+
+
+
+
+@Tag("TestYandexMarkerItemFind")
+class TestYandexMarkerItemFind extends TestBase {
+
+    @Test
+    @DisplayName("Найти в магазине Яндекс.Маркет заданный элемент с возможностью подставить свой через терминал")
+    void findItem() {
+
+
+    }
+}
